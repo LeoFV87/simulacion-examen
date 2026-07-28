2eb3a7731a9e952cbcbb9d0cb068c77d6e743ecb
LeoFV87 <leoelprogamer92@gmail.com>
2026-07-27 21:07:32 -0500

    Solucion del build para deploy exitoso

--- server.test.js
+++ server.test.js
@@ -4,7 +4,7 @@ const app = require('./server');
 describe('Pruebas HTTP', () => {
   it('GET / debe responder con status 200 y mensaje ok', async () => {
     const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(500);
+    expect(res.statusCode).toEqual(200);
     expect(res.body.status).toEqual('ok');
   });
 });
